ddfca87ac4765bcb8c9657419d2338c4d099dec5
Vinson Chuong <vinsonchuong@gmail.com>
2015-11-15 15:26:22 -0800

    Fix linting error

--- src/bin/eslint.js
+++ src/bin/eslint.js
@@ -182,7 +182,9 @@ const options = {
       'sort-vars': 0,
       'space-after-keywords': 2,
       'space-before-blocks': 2,
+
       // 'space-before-function-paren': [2, 'never'],
+
       'space-in-parens': 2,
       'space-infix-ops': [2, {int32Hint: true}],
       'space-return-throw-case': 2,
