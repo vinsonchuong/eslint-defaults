a89a0477669ceae4d9f51bc4bc89e4e3f56bad6b
Vinson Chuong <vinsonchuong@gmail.com>
2016-03-18 13:09:27 -0700

    Start to introduce directory-helpers

--- spec/bin/eslint_spec.js
+++ spec/bin/eslint_spec.js
@@ -1,21 +1,11 @@
 import * as path from 'path';
-import {fs, childProcess} from 'node-promise-es6';
-import * as fse from 'fs-extra-promise-es6';
+import {childProcess} from 'node-promise-es6';
+import Directory from 'directory-helpers';
 
 const eslintBin = path.resolve('node_modules/eslint/bin/eslint.js');
 const eslintDefaultsBin = path.resolve('node_modules/.bin/eslint');
 
-class Project {
-  constructor() {
-    /* eslint-disable lines-around-comment, no-sync */
-    fs.mkdirSync(this.path());
-    /* eslint-enable lines-around-comment, no-sync */
-  }
-
-  path(...args) {
-    return path.resolve('project', ...args);
-  }
-
+class Project extends Directory {
   async exec(command) {
     const {stdout, stderr} = await childProcess.exec(`${command} || true`, {
       cwd: this.path()
@@ -36,133 +26,134 @@ class Project {
     const expected = await this.eslint(eslintArgs || eslintDefaultArgs);
     expect(actual).toBe(expected);
   }
-
-  async write(file, data) {
-    if (typeof data === 'object') {
-      await fse.writeJson(this.path(file), data);
-    } else {
-      const lines = data.split('\n');
-      const indent = lines[1].match(/^\s*/)[0].length;
-      const indentedCode = lines
-        .map((line) => line.slice(indent))
-        .join('\n')
-        .trim();
-      await fs.writeFile(this.path(file), `${indentedCode}\n`);
-    }
-  }
 }
 
 describe('eslint', () => {
   afterEach(async () => {
-    await fse.remove('project');
+    await new Project('project').remove();
   });
 
   it('preserves CLI flags', async () => {
-    const project = new Project();
+    const project = new Project('project');
+    await project.create();
     await project.expectSameOutput('-h');
   });
 
   it('can lint ES5', async () => {
-    const project = new Project();
-    await project.write('_eslintrc', {
-      rules: {
-        'no-debugger': 2
-      }
+    const project = new Project('project');
+    await project.write({
+      '_eslintrc': {
+        rules: {
+          'no-debugger': 2
+        }
+      },
+      'index.js': `
+        debugger;
+      `
     });
-    await project.write('index.js', `
-      debugger;
-    `);
     await project.expectSameOutput('index.js');
   });
 
   it('can lint ES.next', async () => {
-    const project = new Project();
-    await project.write('_eslintrc', {
-      parser: 'babel-eslint'
+    const project = new Project('project');
+    await project.write({
+      '_eslintrc': {
+        parser: 'babel-eslint'
+      },
+      'index.js': `
+        import * as path from 'path';
+        path.resolve();
+      `
     });
-    await project.write('index.js', `
-      import * as path from 'path';
-      path.resolve();
-    `);
     await project.expectSameOutput('index.js');
   });
 
   it('can lint async functions with generator-star-spacing', async () => {
-    const project = new Project();
-    await project.write('_eslintrc', {
-      parser: 'babel-eslint',
-      plugins: ['babel'],
-      rules: {
-        'babel/generator-star-spacing': [2, 'after']
-      }
+    const project = new Project('project');
+    await project.write({
+      '_eslintrc': {
+        parser: 'babel-eslint',
+        plugins: ['babel'],
+        rules: {
+          'babel/generator-star-spacing': [2, 'after']
+        }
+      },
+      'index.js': `
+        async function run() {
+          await Promise.resolve(42);
+        }
+        run();
+      `
     });
-    await project.write('index.js', `
-      async function run() {
-        await Promise.resolve(42);
-      }
-      run();
-    `);
     await project.expectSameOutput('index.js');
   });
 
   it('can lint export statements with object-curly-spacing', async () => {
-    const project = new Project();
-    await project.write('_eslintrc', {
-      parser: 'babel-eslint',
-      plugins: ['babel'],
-      rules: {
-        'babel/object-curly-spacing': 2
-      }
+    const project = new Project('project');
+    await project.write({
+      '_eslintrc': {
+        parser: 'babel-eslint',
+        plugins: ['babel'],
+        rules: {
+          'babel/object-curly-spacing': 2
+        }
+      },
+      'index.js': `
+        export * from 'fs';
+        export * as fse from 'fs';
+      `
     });
-    await project.write('index.js', `
-      export * from 'fs';
-      export * as fse from 'fs';
-    `);
     await project.expectSameOutput('index.js');
   });
 
   it('can lint object spread with object-shorthand', async () => {
-    const project = new Project();
-    await project.write('_eslintrc', {
-      parser: 'babel-eslint',
-      plugins: ['babel'],
-      rules: {
-        'babel/object-shorthand': 2
-      }
+    const project = new Project('project');
+    await project.write({
+      '_eslintrc': {
+        parser: 'babel-eslint',
+        plugins: ['babel'],
+        rules: {
+          'babel/object-shorthand': 2
+        }
+      },
+      'index.js': `
+        const {one, two, ...rest} = {one: 1, two: 2, three: 3, four: 4};
+        JSON.stringify({one, two, ...rest});
+      `
     });
-    await project.write('index.js', `
-      const {one, two, ...rest} = {one: 1, two: 2, three: 3, four: 4};
-      JSON.stringify({one, two, ...rest});
-    `);
     await project.expectSameOutput('index.js');
   });
 
   it('defaults to linting the current directory', async () => {
-    const project = new Project();
-    await project.write('_eslintrc', {
-      rules: {
-        'no-debugger': 2
-      }
+    const project = new Project('project');
+    await project.write({
+      '_eslintrc': {
+        rules: {
+          'no-debugger': 2
+        }
+      },
+      'index.js': `
+        debugger;
+      `
     });
-    await project.write('index.js', `
-      debugger;
-    `);
     await project.expectSameOutput('', 'index.js');
   });
 
   it('ignores files listed in .gitignore', async () => {
-    const project = new Project();
-    await project.write('_eslintrc', {
-      rules: {
-        'no-debugger': 2
-      }
+    const project = new Project('project');
+    await project.write({
+      '_eslintrc': {
+        rules: {
+          'no-debugger': 2
+        }
+      },
+      '.gitignore': `
+        index.js
+      `,
+      'index.js': `
+        debugger;
+      `
     });
-    await project.write('.gitignore', `
-      index.js
-    `);
-    await project.write('index.js', `
-      debugger;
-    `);
     await project.expectSameOutput('', '--ignore-path .gitignore .');
   });
 });
